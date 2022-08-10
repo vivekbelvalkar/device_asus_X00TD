ba8ccae792a824d93e715cdd7edb497d3080bdec
vivekbelvalkar <vvkbel@gmail.com>
2022-08-10 18:47:42 +0530

    X00TD:  Replace audio@2.0-service with audio.service

--- device.mk
+++ device.mk
@@ -25,7 +25,7 @@ OVERRIDE_TARGET_FLATTEN_APEX := true
 # Audio
 PRODUCT_PACKAGES += \
     android.hardware.audio@6.0-impl \
-    android.hardware.audio@2.0-service \
+    android.hardware.audio.service \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.effect@2.0-service
 
